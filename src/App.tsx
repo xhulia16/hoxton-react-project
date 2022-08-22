4040fbfdf1439425a963bc46abd7b9ada6b6e966
XHULIA PRISHKA <xh.prishka@gmail.com>
2022-08-22 18:37:05 +0200

    slider

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,57 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
+import { Article } from "./types";
 
 function App() {
+  const [article, setArticle] = useState<null | Article[]>(null);
+  const[index, setIndex]=useState(0)
+
+  useEffect(() => {
+    fetch("http://localhost:4000/articles")
+      .then((resp) => resp.json())
+      .then((articlesFromServer) => setArticle(articlesFromServer));
+  }, []);
+
+   if(article===null)
+   return <h1>Loading...</h1>
+
+   function previous(){
+    if(index===0){
+      let newIndex=article.length-1
+      setIndex(newIndex)
+    }
+    else 
+    setIndex(index-1)
+   }
+
+   function next(){
+    if(index===article.length-1){
+      let newIndex=0
+      setIndex(newIndex)
+    }
+    else 
+    setIndex(index+1)
+   }
+
+window.article=article
   return (
     <div className="App">
       <Header></Header>
       <main>
-        <span className="material-symbols-outlined">
+        <span onClick={next}
+        className="material-symbols-outlined">
           keyboard_double_arrow_left
         </span>
-        <article>
-          <img src="https://www.nasa.gov/sites/default/files/thumbnails/image/51956990663_a7a5cad860_k_0.jpg"></img>
-          <h3>NASA Sets Launch Coverage for Artemis Mega Moon Rocket, Spacecraft</h3>
-        </article>
-        <span className="material-symbols-outlined">
+        <div className="article-container">
+            <article>
+              <img src={article[0 + index].image}></img>
+              <h3>{article[0 +index].title}</h3>
+            </article>
+        </div>
+        <span onClick={previous}
+        className="material-symbols-outlined">
           keyboard_double_arrow_right
         </span>
       </main>
