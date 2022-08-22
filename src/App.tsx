aa201a476798588c54cd5722f8d26f1e43d9adca
XHULIA PRISHKA <xh.prishka@gmail.com>
2022-08-22 19:10:55 +0200

    commit css

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { Article } from "./types";
 
 function App() {
   const [article, setArticle] = useState<null | Article[]>(null);
-  const[index, setIndex]=useState(0)
+  const [index, setIndex] = useState(0);
 
   useEffect(() => {
     fetch("http://localhost:4000/articles")
@@ -14,46 +14,45 @@ function App() {
       .then((articlesFromServer) => setArticle(articlesFromServer));
   }, []);
 
-   if(article===null)
-   return <h1>Loading...</h1>
+  if (article === null) return <h1>Loading...</h1>;
 
-   function previous(){
-    if(index===0){
-      let newIndex=article.length-1
-      setIndex(newIndex)
-    }
-    else 
-    setIndex(index-1)
-   }
+  function previous() {
+    if (index === 0) {
+      let newIndex = article.length - 1;
+      setIndex(newIndex);
+    } else setIndex(index - 1);
+  }
 
-   function next(){
-    if(index===article.length-1){
-      let newIndex=0
-      setIndex(newIndex)
-    }
-    else 
-    setIndex(index+1)
-   }
+  function next() {
+    if (index === article.length - 1) {
+      let newIndex = 0;
+      setIndex(newIndex);
+    } else setIndex(index + 1);
+  }
 
-window.article=article
   return (
     <div className="App">
       <Header></Header>
       <main>
-        <span onClick={next}
-        className="material-symbols-outlined">
-          keyboard_double_arrow_left
-        </span>
-        <div className="article-container">
-            <article>
-              <img src={article[0 + index].image}></img>
-              <h3>{article[0 +index].title}</h3>
-            </article>
+        <div className="main-text">
+          <h2>EXPLORE </h2>
+          <h1>LATEST NEWS</h1>
         </div>
-        <span onClick={previous}
-        className="material-symbols-outlined">
-          keyboard_double_arrow_right
-        </span>
+        <div>
+          <figure className="star-figure"></figure>
+          </div>
+        <div className="article-container">
+          <span onClick={next} className="material-symbols-outlined">
+            keyboard_double_arrow_left
+          </span>
+          <article>
+            <img src={article[0 + index].image}></img>
+            <h3>{article[0 + index].title}</h3>
+          </article>
+          <span onClick={previous} className="material-symbols-outlined">
+            keyboard_double_arrow_right
+          </span>
+        </div>
       </main>
       <Footer></Footer>
     </div>
