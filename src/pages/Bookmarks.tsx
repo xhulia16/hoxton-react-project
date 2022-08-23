3ea7f8add6715e4214d6f316ebf67277f4d80693
XHULIA PRISHKA <xh.prishka@gmail.com>
2022-08-24 00:20:40 +0200

    display bookmarks

--- src/pages/Bookmarks.tsx
+++ src/pages/Bookmarks.tsx
@@ -1,7 +1,29 @@
-export function Bookmarks(){
-    return(
-        <div>
-            <h1>Here are your bookmarks</h1>
-        </div>
-    )
+import { useEffect, useState } from "react";
+import { Bookmark } from "../types";
+
+export function Bookmarks() {
+  const [bookmarks, setBookmarks] = useState<Bookmark[]>([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/bookmarks/?_expand=article`)
+      .then((resp) => resp.json())
+      .then((bookmarks) => setBookmarks(bookmarks));
+  }, []);
+  return (
+    <div className="bookmarks-container">
+      <h1>Your Bookmarks</h1>
+      <ul>
+        {bookmarks.map((item) => (
+          <li className="bookmark-detail">
+            <img src={item.article.image}></img>
+            <div>
+            <h2>{item.article.title}</h2>
+            <p>{item.article.description}</p>
+            </div>
+            <button>Remove</button>
+            </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
