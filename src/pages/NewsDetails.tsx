72e8a03eb58932551c329376d9f31b7d0dc9b2a9
XHULIA PRISHKA <xh.prishka@gmail.com>
2022-08-24 02:08:40 +0200

    check if the bookmark already exists

--- src/pages/NewsDetails.tsx
+++ src/pages/NewsDetails.tsx
@@ -1,48 +1,74 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { Article, Bookmark } from "../types";
 
-export function NewsDetails(){
-    const params = useParams();
-    const navigate=useNavigate();
-    const [singleArticle, setSingleArticle]=useState<Article | null>();
+export function NewsDetails() {
+  const params = useParams();
+  const navigate = useNavigate();
+  const [singleArticle, setSingleArticle] = useState<Article | null>();
+  const [bookmarks, setBookmarks] = useState<Number[]>([]);
 
+  useEffect(() => {
+    fetch(`http://localhost:4000/articles/${params.itemId}`)
+      .then((resp) => resp.json())
+      .then((articleFromServer) => setSingleArticle(articleFromServer));
+  }, []);
 
-    useEffect(()=>{
-        fetch(`http://localhost:4000/articles/${params.itemId}`)
-        .then(resp=>resp.json())
-        .then(articleFromServer=> setSingleArticle(articleFromServer))
-    }, [])
+  useEffect(() => {
+    fetch(`http://localhost:4000/bookmarks`)
+      .then((resp) => resp.json())
+      .then((bookmarks) =>
+        setBookmarks(bookmarks.map((item) => item.articleId))
+      );
+  }, []);
 
-    function addBookmark(){
-        fetch("http://localhost:4000/bookmarks",{
-            method: "POST", 
-            headers: {
-                "Content-type": "application/json"
-            }, 
-            body: JSON.stringify({
-                articleId: singleArticle?.id
-            })
-            
-        })
-        .then(data=> navigate('/bookmarks'))
-     }
-    
- 
+  function addBookmark() {
+    // for(let bookmark of bookmarks) {
+    //     if(singleArticle?.id!== bookmark.articleId)
+    if (
+      bookmarks.every((id) => {
+        return id !== singleArticle?.id;
+      })
+    ) {
+      {
+        fetch("http://localhost:4000/bookmarks", {
+          method: "POST",
+          headers: {
+            "Content-type": "application/json",
+          },
+          body: JSON.stringify({
+            articleId: singleArticle?.id,
+          }),
+        }).then((data) => navigate("/bookmarks"));
+      }
+    }
+    else {
+        alert("This is already on your bookmarks!")
+    }
+  }
 
-    return(
-<div className="details-container">
-    <img src={singleArticle?.image}></img>
-   <div className="details">
-    <h2>{singleArticle?.title}</h2>
-    <p>{singleArticle?.description}</p>
-    <button onClick={addBookmark}
-    >Bookmark</button>
-    <input placeholder="Leave a comment..."></input>
-    <ul>
-        <li>List of comments here</li>
-    </ul>
-    </div> 
-</div>
-    )
+  return (
+    <div className="details-container">
+      <img src={singleArticle?.image}></img>
+      <div className="details">
+        <h2>{singleArticle?.title}</h2>
+        <p>{singleArticle?.description}</p>
+        <button
+          onClick={() => {
+            let newBookmark = {
+              articleId: singleArticle?.id,
+            };
+            setBookmarks([...bookmarks, newBookmark]);
+            addBookmark();
+          }}
+        >
+          Bookmark
+        </button>
+        <input placeholder="Leave a comment..."></input>
+        <ul>
+          <li>List of comments here</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
